6f5b34208793ba5d29fc9447891e4e5dcb98512d
venumadhavik <123043484+venumadhavik@users.noreply.github.com>
2023-04-10 23:16:11 -0500

    Create api.js

---
+++ controllers/api.js
@@ -0,0 +1,10 @@
+// API for our resources
+exports.api = function(req, res) {
+    res.write('[');
+    res.write('{"resource":"car", ');
+    res.write(' "verbs":["GET","PUT", "DELETE"] ');
+    res.write('}');
+    res.write(']')
+    res.send();
+    };
+    
